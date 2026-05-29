d7c01d795c724b02680b2d577adeefc994b60bfa
theinfn8 <ccarson@email.com>
2026-05-29 19:43:14 +0200

    Pulled row parsing into individual functions; saved original html added looping for entire page; added looping for multiple pages; export to json; added llm directory and code

--- module_2/scrape.py
+++ module_2/scrape.py
@@ -1,11 +1,14 @@
 import sys
 from urllib3 import PoolManager
 from bs4 import BeautifulSoup
-
+import json
+import time
+import random
 
 basePageURL = 'https://www.thegradcafe.com/survey?page='
 basePageURLSortOrder = '&sort=newest'
 baseURL = 'https://www.thegradcafe.com'
+http = PoolManager()
 
 cols = [
         "id",
@@ -24,46 +27,59 @@ cols = [
         "gpa",
         "gre",
         "grev",
-        "greaw"
+        "greaw",
+        "html_orig1",
+        "html_orig2",
+        "html_orig3"
     ]
 
 def _newRecord():
     return dict.fromkeys(cols, "None")
+
+def _fetchPage(pageNum):
+    # Build in wait time for politeness
+    time.sleep(random.randint(0,5))
+    
+    url = basePageURL + str(pageNum) + basePageURLSortOrder
+    response = http.request('GET', url)
+    status = response.status
+
+    if status != 200:
+        if status == 400:
+            print("Bad Request on Base Page")
+        elif status == 404:
+            print("Base Page not found")
+        else:
+            print(f"An HTTP error has occured: {status}")
+        sys.exit(1)
+    return response.data
 
 def _processPrimaryRow(tr, tempDict):
     tableData = tr.find_all("td")
     # Row 1 field order = 0: University, 1: Degree Name and Type, 2: Date Added, 3: Status and Date of Status, 4: ID/URL
     
     # 0 University
-    print(tableData[0].text)
     tempDict["university"] = str.strip(tableData[0].text)
 
     # 1 Degree Name and Type
     # Each element is in their own span tag
     spans = tableData[1].find_all("span")
-    print(spans[0].text)
-    print(spans[1].text)
     tempDict["program"] = str.strip(spans[0].text)
     tempDict["degree"] = str.strip(spans[1].text)
 
     # 2 Date added
-    print(tableData[2].text)
     tempDict["date_added"] = str.strip(tableData[2].text)
 
     # 3 Statuses: Accepted, Rejected, Interview, Wait listed (Uniformity in the entry implies this is selected from a list)
-    print(tableData[3].text)
     splitStatus = str.split(tableData[3].text, " on ")
     tempDict["status"] = str.strip(splitStatus[0])
-    print(splitStatus[0])
     tempDict["status_date"] = str.strip(splitStatus[1])
 
     # 4 ID and specific URL
     # Located in anchor tag, so grab the "a" and read the href
     anchor = tableData[4].find("a")
-    print(anchor["href"])
     tempDict["url"] = baseURL + anchor["href"]
     # We can pull out the ID so we have a unique identifier for later, so we might as well
-    print("ID: " + str.split(anchor["href"], "/")[2])
     tempDict["id"] = str.split(anchor["href"], "/")[2]
 
 def _processSecondaryRow(tr, tempDict):
@@ -72,11 +88,9 @@ def _processSecondaryRow(tr, tempDict):
     # 1 div repeats status info, but is hidden ** Skip it
 
     # 2 Term
-    print(divs[2].text)
     tempDict["term"] = divs[2].text
 
     # 3 US or International
-    print(divs[3].text)
     tempDict["US/International"] = divs[3].text
 
     # If there are no other flex boxes, exit
@@ -85,7 +99,6 @@ def _processSecondaryRow(tr, tempDict):
     
     # Else the complicated parse
     for i in range(4, len(divs)):
-        print(divs[i].text)
         parsedBox = str.split(divs[i].text)
 
         # If we have two items it should be GPA or GRE
@@ -107,45 +120,54 @@ def _processSecondaryRow(tr, tempDict):
 
 def scrape_data():
     
-    numPagesToRead = 1
-    currentPage = 1
+    # 30,000 entries at 20 Entries per page is 1500 pages
+    numPagesToRead = 5
+    #currentPage = 1
 
-    # Create connection pool manager
-    http = PoolManager()
-    url = basePageURL + str(currentPage) + basePageURLSortOrder
-    print(url)
+    data = list()
 
-    response = http.request('GET', url)
-    status = response.status
+    for page in range(1, numPagesToRead):
+        print("Page: {page}")
+        parsedPage = BeautifulSoup(_fetchPage(page), "html.parser")
 
-    if status != 200:
-        if status == 400:
-            print("Bad Request on Base Page")
-        elif status == 404:
-            print("Base Page not found")
-        else:
-            print(f"An HTTP error has occured: {status}")
-        sys.exit(1)
+        tableBody = parsedPage.find("tbody")
+        tableRows = tableBody.find_all("tr")
 
-    parsedPage = BeautifulSoup(response.data, "html.parser")
+        i = 0
+        entryCount = 0
+        while i < len(tableRows):
+            # Skip ad rows halfway through
+            if entryCount == 10:
+                i += 2
 
-    tableBody = parsedPage.find("tbody")
-    #print(tableData)
-    tableRows = tableBody.find_all("tr")
+            tempDict = _newRecord()
+            _processPrimaryRow(tableRows[i], tempDict)
+            tempDict["html_orig1"] = str(tableRows[i].contents)
+            i += 1
 
-    tempDict = _newRecord()
-    _processPrimaryRow(tableRows[0], tempDict)
-    print(tempDict["id"])
+            _processSecondaryRow(tableRows[i], tempDict)
+            tempDict["html_orig2"] = str(tableRows[i].contents)
+            i += 1
 
-    _processSecondaryRow(tableRows[1], tempDict)
+            # Is there a third row with comments?
+            p = tableRows[i].find("p")
+            if p != None:
+                tempDict["comments"] = p.text
+                tempDict["html_orig3"] = str(tableRows[i].contents)
+                i += 1
 
-    # Is there a third row with comments?
-    p = tableRows[2].find("p")
-    if len(p) == 1:
-        print(p.text)
-        tempDict["comments"] = p.text
+            data.append(tempDict)
 
-    print(tempDict)
-    
+            
+            entryCount += 1
+            
+            # And skip the last ad row
+            if entryCount == 20:
+                i += 1
+
+        
+
+    with open("applicant_data.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(data))
 
     
