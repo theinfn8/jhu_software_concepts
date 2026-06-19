79bbf0f8db2f2c41210dac87f7507be6e9bf2dbc
theinfn8 <ccarson@email.com>
2026-06-19 22:27:43 +0200

    More CI pipeline testing

--- module_5/src/config.py
+++ module_5/src/config.py
@@ -31,16 +31,10 @@ def get_config(testconfig=None):
 
     if testconfig is None:
         load_dotenv("./.env")
-        config = {
-            "host" : os.getenv("DB_HOST"),
-            "port" : os.getenv("DB_PORT"),
-            "dbname" : os.getenv("DB_NAME"),
-            "user" : os.getenv("DB_USER"),
-            "password" : os.getenv("DB_PASSWORD")
-        }
     else:
         load_dotenv("../tests/.env")
-        config = {
+
+    config = {
                 "host" : os.getenv("DB_HOST"),
                 "port" : os.getenv("DB_HOST"),
                 "dbname" : os.getenv("DB_HOST"),
